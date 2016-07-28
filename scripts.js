3705a6cf8d7a9f6001805663125b036d878c97cb
hsoltanim <hsoltanim@gmail.com>
2016-07-28 10:24:34 +0430

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,75 +3,46 @@ function putfields() {
 	var aeml= ' <a hr'; aeml += 'ef="m'; aeml += 'ail'; aeml += 'to:'; aeml += eml; aeml += '">'; aeml += eml; aeml += '</a> '
     
 	document.getElementById("topbar").innerHTML = '\
-<div class="bit-100">\
-	<a href="#" class="nav-title">\
+<div class="sidesection">\
+	<h1><a href="#">\
 		درباره سازه ها ... \
-	</a>\
-	<a href="index.html" class="nav-link">\
-		خانه\
-	</a>\
-	<a href="about.html" class="nav-link">\
-		درباره\
-	</a>\
+	</a></h2>\
 	<br>\
-</div>\
-';
-	
-	document.getElementById("sidebar").innerHTML = '\
-	<div class="bit-25">\
+	<div class="sideinfo">\
 		<br>\
 		<h2>\
 			نوشته ها\
 		</h2>\
 		<p><a href="isolation.html">\
 			جداسازی پایه\
-		</a></p><hr>\
+		</a></p>\
 		<p><a href="outrigger.html">\
 			سیستم برون بند\
-		</a></p><hr>\
+		</a></p>\
 		<p><a href="brbf.html">\
 			مهاربندهای کمانش تاب\
-		</a></p><hr>\
+		</a></p>\
 		<p><a href="ebf.html">\
 			مهاربندهای واگرا (برون محور)\
-		</a></p><hr>\
+		</a></p>\
 		<p><a href="structuralsystems.html">\
 			روش های نوین طراحی لرزه ای\
-		</a></p><hr>\
+		</a></p>\
 		<p><a href="steelvsconcrete.html">\
 			سازه فولادی یا بتنی؟\
-		</a></p><hr>\
+		</a></p>\
 		<p><a href="about.html">\
 			درباره این وب سایت\
-		</a></p>\
-	</div>\
-';
-	
-	document.getElementById("botbar").innerHTML = '\
-<footer class="footer">\
-	<div class="bit-75">\
+		</a></p><br>\
 		<p>\
-این وب سایت توسط حمید سلطانی محمدی تهیه گردیده است.\
-<br>\
-برای تماس با نگارنده می توانید به آدرس پست الکترونیکی\
+نگارنده: حمید سلطانی محمدی \
+		<br>\
+آدرس ایمیل:\
  ' + aeml + ' \
-مکاتبه فرمایید.\
-<br>\
-استفاده از مطالب این وب سایت آزاد است.\
 		</p>\
 	</div>\
-	<div class="bit-25">\
-		<a href="#header" class="footer-link">\
-			بازگشت به بالا\
-		</a><br>\
-		<a href="index.html">\
-			خانه\
-		</a> - \
-		<a href="about.html">\
-			درباره\
-		</a>\
-	</div>\
-</footer>\
+</div>\
+\
 ';
 
 }
